8ebc55cbbd26f4c71c81c90233c32604822a8fa3
kimt4ehun <kthrlaxo@naver.com>
2017-07-26 16:34:04 +0900

    로그인 기능 구현 및 base 로그인 부분 변경

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 # Create your views here.
 #
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect(settings.LOGIN_URL) # 회원가입에 성공하면,   LOGIN     페이지로 이동
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', { 'form': form,
+        })
+
--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,7 @@ from . import views
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^$', views.all),
+    url(r'^$', views.all, name="home"),
     #url(r'^(?P<id>\d+)/$', views.post_detail),
     #url(r'^post/(?P<id>\d+)/$', views.post_list),
  ]
